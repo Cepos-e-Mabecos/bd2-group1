a45ab22526601fbcef97a92359f1a25620bc3133
EdwinOliveira <39379560+EdwinOliveira@users.noreply.github.com>
2019-12-10 22:14:24 +0000

    Update_Allergies_Api

--- src/api/allergies.py
+++ src/api/allergies.py
@@ -1,37 +1,53 @@
 # Import Packages
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import psycopg2
-# Import Database file
-# from db import create_connection
+import config
+
 # Define Blueprint
 allergy = Blueprint('allergy', __name__)
+
 # Get allergies
 @allergy.route('/', methods=['GET'])
 def get_allergies():
     try:
         # Get connection
         connection = psycopg2.connect(
-            # This will be the server (Since we are using PGAdmin then it's Localhost);
-            host="localhost",
-            database="postgres",  # Keep the same database name,;
-            user="postgres",  # Keep the same name, "postgres"
-            port="5432",
-            password="postgres01"  # Keep the same password, "postgres01";
+            host=config.HOST(),
+            port=config.PORT(),
+            database=config.DB_NAME(),
+            user=config.POSTGRES_USER(),
+            password=config.POSTGRES_PASSWORD()
         )
+
         # Defining the cursor to execute on the database
         cur = connection.cursor()
+
         # Creating the SQL Command
         encoded_command = (
             f"Select * From Select_Allergies")
+
         # Executing the SQL Command
         cur.execute(encoded_command)
-    except:
+
+        # Fetching all the records
+        database_records = cur.fetchall()
+
+        # Json of the records
+        jsonify(database_records)
+
+        # List of Records
+        list_records = []
+
+        for record in database_records:
+            dictionary_row = {
+                "Allergy_Cod": record[0], "Symptom": record[1]
+            }
+            list_records.append(dictionary_row)
+
+    except (Exception, psycopg2.Error) as error:
         return jsonify({
-            "Result": "Error",
-            "Message": "Error with the server!",
-            "Status": "500",
-            "Content-Type": "application/json"
-        })
+            "Result": error
+        }), 500
     finally:
         # commiting and closing database connection.
         if(connection):
@@ -41,31 +57,33 @@ def get_allergies():
             connection.commit()
             # In the end the connection is closed
             connection.close()
+
         return jsonify({
-            "Result": "Success",
-            "Message": "Created with success!",
-            "Status": "201",
-            "Content-Type": "application/json"
-        })
+            "Results": list_records
+        }), 201
+
 # Get allergy
 @allergy.route('/<cod_allergy>', methods=['GET'])
 def get_allergy(cod_allergy):
     try:
-        # Get connection
+       # Get connection
         connection = psycopg2.connect(
-            # This will be the server (Since we are using PGAdmin then it's Localhost);
-            host="localhost",
-            database="postgres",  # Keep the same database name,;
-            user="postgres",  # Keep the same name, "postgres"
-            port="5432",
-            password="postgres01"  # Keep the same password, "postgres01";
+            host=config.HOST(),
+            port=config.PORT(),
+            database=config.DB_NAME(),
+            user=config.POSTGRES_USER(),
+            password=config.POSTGRES_PASSWORD()
         )
+
         # Defining the cursor to execute on the database
         cur = connection.cursor()
+
         # Creating the SQL Command
         encoded_command = ()
+
         # Executing the SQL Command
         cur.execute(encoded_command)
+
     except (Exception, psycopg2.Error) as error:
         return ("Allergy was not created!", error)
     finally:
@@ -78,28 +96,37 @@ def get_allergy(cod_allergy):
             # In the end the connection is closed
             connection.close()
         return "Allergy created with success"
+
 # Post allergy
 @allergy.route('/', methods=['POST'])
-def post_allergy(data_json):
+def post_allergy():
     try:
         # Get connection
         connection = psycopg2.connect(
-            # This will be the server (Since we are using PGAdmin then it's Localhost);
-            host="localhost",
-            database="postgres",  # Keep the same database name,;
-            user="postgres",  # Keep the same name, "postgres"
-            port="5432",
-            password="postgres01"  # Keep the same password, "postgres01";
+            host=config.HOST(),
+            port=config.PORT(),
+            database=config.DB_NAME(),
+            user=config.POSTGRES_USER(),
+            password=config.POSTGRES_PASSWORD()
         )
+
         # Defining the cursor to execute on the database
         cur = connection.cursor()
+
+        # Get the Data as Json
+        data_json = request.get_json()
+
         # Creating the SQL Command
         encoded_command = (
             f"call insert_allergy('{data_json}')")
+
         # Executing the SQL Command
         cur.execute(encoded_command)
+
     except (Exception, psycopg2.Error) as error:
-        return ("Allergy was not created!", error)
+        return jsonify({
+            "Result": error
+        }), 500
     finally:
         # commiting and closing database connection.
         if(connection):
@@ -109,11 +136,55 @@ def post_allergy(data_json):
             connection.commit()
             # In the end the connection is closed
             connection.close()
-        return "Allergy created with success"
+        return jsonify({
+            "Message": "Success"
+        }), 201
+
 # Put allergy
 @allergy.route('/<cod_allergy>', methods=['PUT'])
-def put_allergy(cod_allergy, data_json):
-    return 'Put Method'
+def put_allergy(cod_allergy):
+    try:
+        # Get connection
+        connection = psycopg2.connect(
+            host=config.HOST(),
+            port=config.PORT(),
+            database=config.DB_NAME(),
+            user=config.POSTGRES_USER(),
+            password=config.POSTGRES_PASSWORD()
+        )
+
+        # Defining the cursor to execute on the database
+        cur = connection.cursor()
+
+        # Get the Data as Json
+        data_json = request.get_json()
+
+        # Creating the SQL Command
+        encoded_command = (
+            f"call update_allergy({cod_allergy},'{data_json}')")
+
+        print(encoded_command)
+
+        # Executing the SQL Command
+        cur.execute(encoded_command)
+
+    except (Exception, psycopg2.Error) as error:
+        return jsonify({
+            "Result": error
+        }), 500
+    finally:
+        # commiting and closing database connection.
+        if(connection):
+            # First the cursor will be closed
+            cur.close()
+            # Then the data is commited
+            connection.commit()
+            # In the end the connection is closed
+            connection.close()
+        return jsonify({
+            "Message": "Success"
+        }), 201
+
 # Delete allergy
 @allergy.route('/<cod_allergy>', methods=['DELETE'])
 def delete_allergy(cod_allergy):
--- src/api/config.py
+++ src/api/config.py
@@ -1,19 +1,23 @@
 def DB_NAME():
-    DB_NAME="BD_FINAL"
+    DB_NAME = "postgres"
     return DB_NAME
 
+
 def HOST():
-    HOST="localhost"
+    HOST = "localhost"
     return HOST
 
+
 def PORT():
-    PORT=5432
+    PORT = 5432
     return PORT
 
+
 def POSTGRES_USER():
-    POSTGRES_USER="postgres"
+    POSTGRES_USER = "postgres"
     return POSTGRES_USER
 
+
 def POSTGRES_PASSWORD():
-    POSTGRES_PASSWORD="1250"
+    POSTGRES_PASSWORD = "postgres01"
     return POSTGRES_PASSWORD
